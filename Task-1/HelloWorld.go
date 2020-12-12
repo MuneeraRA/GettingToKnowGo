e36513f78f8c24fc27eb87bfe0f18ab9a64d6c6d
Muneerah <mun.rashed@gmail.com>
2020-12-13 00:30:01 +0300

    Task-1 completed

--- Task-1/HelloWorld.go
+++ Task-1/HelloWorld.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func getInfo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, This is the First Task We work on ")
+}
+func start(w http.ResponseWriter, r *http.Request) {
+	for k, v := range r.URL.Query() {
+		fmt.Fprintf(w, "%s: %s \n", k, v)
+	}
+}
 func main() {
-	http.HandleFunc("/App", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've completed your First Task")
-	})
-
-	http.HandleFunc("/x", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've completed your First Task \n")
-	})
-
-	http.ListenAndServe(":80", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/app", getInfo).Methods("GET")
+	router.HandleFunc("/", start).Methods("GET")
+	http.ListenAndServe(":8083", router)
 }
