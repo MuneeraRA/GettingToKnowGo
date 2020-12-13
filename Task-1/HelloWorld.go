72709beb2f0556d590eaf5e271d3e2610ac26632
Muneerah <mun.rashed@gmail.com>
2020-12-13 22:20:05 +0300

    Fixing

--- Task-1/HelloWorld.go
+++ Task-1/HelloWorld.go
@@ -8,16 +8,21 @@ import (
 )
 
 func getInfo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, This is the First Task We work on ")
+	fmt.Fprintf(w, "Hello, This is the First Task")
 }
-func start(w http.ResponseWriter, r *http.Request) {
-	for k, v := range r.URL.Query() {
-		fmt.Fprintf(w, "%s: %s \n", k, v)
+func print(w http.ResponseWriter, r *http.Request) {
+	if len(r.URL.Query()) == 0 {
+		fmt.Fprintf(w, "Url Params are missing ")
+	} else{
+		for k, v := range r.URL.Query() {
+			fmt.Fprintf(w, "%s: %s \n", k, v)
+		}
 	}
 }
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/app", getInfo).Methods("GET")
-	router.HandleFunc("/", start).Methods("GET")
+	router.PathPrefix("/").HandlerFunc(print)
+	http.Handle("/", router)
 	http.ListenAndServe(":8083", router)
 }
